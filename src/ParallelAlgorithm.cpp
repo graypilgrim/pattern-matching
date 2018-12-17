f9e34aa918f0a8f77ccd87b0d3a36ff5eeb60bd3
Lukasz Wlazly <lukwlazly@gmail.com>
2018-12-17 22:44:38 +0100

    Remove debug info

--- src/ParallelAlgorithm.cpp
+++ src/ParallelAlgorithm.cpp
@@ -2,7 +2,4 @@
 
 ParallelAlgorithm::ParallelAlgorithm(const Text &pattern, const Text &document, size_t threadsNum)
     : Algorithm(pattern, document), threadsNum_(threadsNum)
-{
-    std::cout << "deadbeef " << "ParallelAlgorithm" << std::endl;
-    std::cout << rawPattern_ << std::endl;
-}
+{}
